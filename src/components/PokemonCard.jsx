da98f044da1a6f7e174d2c976429b68819fd2017
Thibaud Dupuis <124290694+ThibaudDps@users.noreply.github.com>
2023-10-12 23:52:26 +0200

    display + count

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,22 +2,25 @@ import PropTypes from "prop-types";
 
 function PokemonCard({ pokemon }) {
   console.log(pokemon);
+  const capitalizeFirstLetter = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
 
   return (
-    <div>
-      <figure>
-        <img
-          className="iconPkmn"
-          src="https://www.pokencyclopedia.info/sprites/menu-icons/ico-a_gbc/ico-a_2_100.gif"
-        />
-        <h2>{pokemon.name.toUpperCase()}</h2>
+    <figure className="card">
+      <img
+        className="iconPkmn"
+        src="https://www.pokencyclopedia.info/sprites/menu-icons/ico-a_gbc/ico-a_2_100.gif"
+      />
+      <h2>{capitalizeFirstLetter(pokemon.name)}</h2>
+      <div className="cardBody">
         {pokemon.imgSrc ? (
-          <img className="imgPkmn" src={pokemon.imgSrc} />
+          <img className="imgPkmn" src={pokemon.imgSrc} className="card-img" />
         ) : (
           <p>???</p>
         )}
-      </figure>
-    </div>
+      </div>
+    </figure>
   );
 }
 
--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import "../styles/Button.css";
 
 const Button = ({ count, setCount }) => {
   return (
-    <div>
+    <div className="clicker">
       <button onClick={() => setCount(count + 1)}>TRAIN</button>
     </div>
   );
